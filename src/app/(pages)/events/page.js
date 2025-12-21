cc010d9c25fc90749d6873f97a7533014708d360
Joey Tibbett <42547944+tibbettja@users.noreply.github.com>
2025-12-21 12:59:12 -0500

    communion event added

--- src/app/(pages)/events/page.js
+++ src/app/(pages)/events/page.js
@@ -16,6 +16,7 @@ import {
 import nextConfig from "next.config.mjs";
 
 const events = [
+  { id: 'comm_122125', src: `${nextConfig.assetPrefix}/images/communion.jpg`, title: 'Communion', subheader: 'Dec 21'},
   { id: '21dop_26', src: 'https://cdn.sanity.io/images/2uj21qja/production/3d5a5bf900f9095c70de86b3a87bc23f6acf515b-2250x2250.jpg', title: '21 Days of Prayer', subheader: 'Jan 4 - Jan 26', learnMore: '/pray'}
 ]
 
