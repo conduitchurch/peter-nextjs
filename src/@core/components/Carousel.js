587ebc8af8cf32491e4e03a80d39c81ccc964fb1
Joey Tibbett <42547944+tibbettja@users.noreply.github.com>
2026-01-17 14:14:27 -0500

    changed transtion behavior for increased performance. preloaded images for landing page.

--- src/@core/components/Carousel.js
+++ src/@core/components/Carousel.js
@@ -1,18 +1,19 @@
 "use client";
 
-import { Box, styled } from "@mui/material";
+import { Box, styled, Chip } from "@mui/material";
 import useEmblaCarousel from "embla-carousel-react";
-import AutoScroll from 'embla-carousel-auto-scroll'
-
+// import AutoScroll from 'embla-carousel-auto-scroll'
+import Autoplay from "embla-carousel-autoplay";
+import Fade from "embla-carousel-fade";
 
 const Embla = styled(Box)(({ theme }) => ({
   overflow: "hidden",
-  width: '100%'
+  width: "100%",
 }));
 
 const EmblaContainer = styled(Box)(({ theme }) => ({
   display: "flex",
-  width: '100%'
+  width: "100%",
 }));
 
 const EmblaSlide = styled(Box)(({ theme }) => ({
@@ -20,19 +21,54 @@ const EmblaSlide = styled(Box)(({ theme }) => ({
   minWidth: 0,
 }));
 
+const EmblaControls = styled(Box)(({ theme }) => ({
+  display: "grid",
+  gridTemplateColumns: "auto 1fr",
+  justifyContent: "space-between",
+  gap: "1.2rem",
+  marginTop: "1.8rem",
+}));
+
+const EmblaDots = styled(Box)(({ theme }) => ({
+  display: "flex",
+  flexWrap: "wrap",
+  justifyContent: "flex-end",
+  alignItems: "center",
+  marginRight: "calc((2.6rem - 1.4rem) / 2 * -1)",
+}));
+
+const DotButton = styled(Chip)(({ theme }) => ({
+  height: ".75em",
+  width: ".75em",
+}));
 
 const Carousel = ({ slides }) => {
-  const [emblaRef] = useEmblaCarousel({ loop: true }, [AutoScroll({ startDelay: 0, })]);
-
+  const [emblaRef] = useEmblaCarousel({ loop: true }, [
+    Autoplay({ delay: 5000 }),
+    Fade()
+  ]);
 
   return (
-    <Embla ref={emblaRef}>
-      <EmblaContainer>
-        { slides.map((slide, i) => (
-          <EmblaSlide key={i}>{ slide }</EmblaSlide>
-        ))}
-      </EmblaContainer>
-    </Embla>
+    <>
+      <Embla ref={emblaRef}>
+        <EmblaContainer>
+          {slides.map((slide, i) => (
+            <EmblaSlide key={i}>{slide}</EmblaSlide>
+          ))}
+        </EmblaContainer>
+      </Embla>
+      {/* <EmblaControls>
+        <EmblaDots>
+          {scrollSnaps.map((_, index) => (
+            <DotButton
+              key={index}
+              onClick={() => onDotButtonClick(index)}
+              color={index === selectedIndex ? "bright" : "gray"}
+            />
+          ))}
+        </EmblaDots>
+      </EmblaControls> */}
+    </>
   );
 };
 
--- src/app/page.js
+++ src/app/page.js
@@ -16,6 +16,7 @@ import {
   useMediaQuery
 } from "@mui/material";
 import nextConfig from "next.config.mjs";
+import Head from "next/head";
 import NextLink from "next/link";
 
 const Slide = styled("img")(({ theme }) => ({
@@ -30,117 +31,136 @@ const Home = () => {
   const theme = useTheme()
   const isMobile = useMediaQuery(theme.breakpoints.down('md'))
   return (
-    <Grid container padding={0} rowSpacing={5} columnSpacing={0}>
-      <Grid size={12} marginTop={isMobile ? 10 : null}>
-        <Carousel
-          slides={[
-            <Slide src="images/church_2.jpg" style={{ objectPosition: '50% 30%' }} />,
-            <Slide src="images/worship_1.jpg" style={{ objectPosition: '50% 0%' }} />,
-            <Slide src="images/family.jpg" style={{ objectPosition: '50% 30%' }} />,
-          ]}
+    <>
+      <Head>
+        <link
+          rel='preload'
+          href='https://theconduit.church/images/church_2.jpg'
+          as='image'
         />
-      </Grid>
-      <Grid size={10} offset={1} sx={{ zIndex: 1 }}>
-        <Grid container spacing={3} alignItems="stretch">
-          <Grid
-            size={{ xs: 12, md: 5 }}
-            offset={{ xs: 0, md: 1 }}
-            display="flex"
-          >
-            <Card sx={{ width: "100%" }}>
-              <CardHeader title="Plan your Visit" />
+        <link
+          rel='preload'
+          href='https://theconduit.church/images/worship_1.jpg'
+          as='image'
+        />
+        <link
+          rel='preload'
+          href='https://theconduit.church/images/family.jpg'
+          as='image'
+        />
+      </Head>
+      <Grid container padding={0} rowSpacing={5} columnSpacing={0}>
+        <Grid size={12} marginTop={isMobile ? 10 : null}>
+          <Carousel
+            slides={[
+              <Slide src="images/church_2.jpg" style={{ objectPosition: '50% 30%' }} />,
+              <Slide src="images/worship_1.jpg" style={{ objectPosition: '50% 0%' }} />,
+              <Slide src="images/family.jpg" style={{ objectPosition: '50% 30%' }} />,
+            ]}
+          />
+        </Grid>
+        <Grid size={10} offset={1} sx={{ zIndex: 1 }}>
+          <Grid container spacing={3} alignItems="stretch">
+            <Grid
+              size={{ xs: 12, md: 5 }}
+              offset={{ xs: 0, md: 1 }}
+              display="flex"
+            >
+              <Card sx={{ width: "100%" }}>
+                <CardHeader title="Plan your Visit" />
+                <CardContent>
+                  <Typography variant="body1">
+                    We'd love for you to join us this Sunday!
+                  </Typography>
+                </CardContent>
+                <CardActions disableSpacing>
+                  <NextLink href="/visit" title="Meet with us">
+                    <Button variant="contained">Meet with us</Button>
+                  </NextLink>
+                </CardActions>
+              </Card>
+            </Grid>
+            {/* <Grid size={{ xs: 12, md: 4 }} display='flex'>
+              <Card sx={{ width: '100%' }}>
+                <CardHeader title="Recent Messages" />
+                <CardContent>
+                  <Typography variant="body1">
+                    Check out our recent messages or full worship service online!
+                  </Typography>
+                </CardContent>
+                <CardActions disableSpacing>
+                  <Button variant="contained">Watch Now</Button>
+                </CardActions>
+                </Card>
+              </Grid> */}
+            {/* <Grid size={{ xs: 12, md: 4 }} display='flex'>
+              <Card sx={{ width: '100%' }}>
+              <CardHeader title="One Year Bible" />
               <CardContent>
                 <Typography variant="body1">
-                  We'd love for you to join us this Sunday!
+                  Spend time in God's Word with this daily Bible reading plan.
                 </Typography>
               </CardContent>
               <CardActions disableSpacing>
-                <NextLink href="/visit" title="Meet with us">
-                  <Button variant="contained">Meet with us</Button>
-                </NextLink>
-              </CardActions>
-            </Card>
-          </Grid>
-          {/* <Grid size={{ xs: 12, md: 4 }} display='flex'>
-            <Card sx={{ width: '100%' }}>
-              <CardHeader title="Recent Messages" />
-              <CardContent>
-                <Typography variant="body1">
-                  Check out our recent messages or full worship service online!
-                </Typography>
-              </CardContent>
-              <CardActions disableSpacing>
-                <Button variant="contained">Watch Now</Button>
+                <Button variant="contained">Start Reading</Button>
               </CardActions>
               </Card>
             </Grid> */}
-          {/* <Grid size={{ xs: 12, md: 4 }} display='flex'>
-            <Card sx={{ width: '100%' }}>
-            <CardHeader title="One Year Bible" />
-            <CardContent>
-              <Typography variant="body1">
-                Spend time in God's Word with this daily Bible reading plan.
+            <Grid size={{ xs: 12, md: 5 }} display="flex">
+              <Card sx={{ width: "100%" }}>
+                <CardHeader title="21 Days of Prayer" />
+                <CardContent>
+                  <Typography variant="body1">
+                    Prayer is our first response, not our last resort.
+                  </Typography>
+                </CardContent>
+                <CardActions disableSpacing>
+                  <NextLink href="/pray" title="Learn More">
+                    <Button variant="contained">Learn More</Button>
+                  </NextLink>
+                </CardActions>
+              </Card>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid size={12} sx={{ zIndex: 1 }} component={Paper}>
+          <Grid container spacing={3} padding={10}>
+            <Grid
+              offset={{ xs: 0, sm: 0, md: 1, lg: 2 }}
+              size={{ xs: 12, sm: 12, md: 6, lg: 5, xl: 3 }}
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <Box sx={{ height: 300, width: 300, overflow: 'hidden' }}>
+                <img
+                  src={`${nextConfig.assetPrefix}/images/pastor.jpg`}
+                  alt="Preaching"
+                  style={{ height: '150%', width: 'auto', marginLeft: '-53%', marginTop: '-20%' }}
+                />
+              </Box>
+            </Grid>
+            <Grid
+              size={{ xs: 12, sm: 12, md: 5, lg: 5, xl: 5 }}
+              sx={{
+                display: "flex",
+                justifyContent: "flex-start",
+                alignItems: "center",
+              }}
+            >
+              <Typography variant="h6">
+                Our mission is to make disciples who are deeply connected to
+                Christ and each other; who are empowered by the Holy Spirit to
+                love boldly; who are equipped to serve as conduits of God's love
+                to a broken world.
               </Typography>
-            </CardContent>
-            <CardActions disableSpacing>
-              <Button variant="contained">Start Reading</Button>
-            </CardActions>
-            </Card>
-          </Grid> */}
-          <Grid size={{ xs: 12, md: 5 }} display="flex">
-            <Card sx={{ width: "100%" }}>
-              <CardHeader title="21 Days of Prayer" />
-              <CardContent>
-                <Typography variant="body1">
-                  Prayer is our first response, not our last resort.
-                </Typography>
-              </CardContent>
-              <CardActions disableSpacing>
-                <NextLink href="/pray" title="Learn More">
-                  <Button variant="contained">Learn More</Button>
-                </NextLink>
-              </CardActions>
-            </Card>
+            </Grid>
           </Grid>
         </Grid>
       </Grid>
-      <Grid size={12} sx={{ zIndex: 1 }} component={Paper}>
-        <Grid container spacing={3} padding={10}>
-          <Grid
-            offset={{ xs: 0, sm: 0, md: 1, lg: 2 }}
-            size={{ xs: 12, sm: 12, md: 6, lg: 5, xl: 3 }}
-            sx={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <Box sx={{ height: 300, width: 300, overflow: 'hidden' }}>
-              <img
-                src={`${nextConfig.assetPrefix}/images/pastor.jpg`}
-                alt="Preaching"
-                style={{ height: '150%', width: 'auto', marginLeft: '-53%', marginTop: '-20%' }}
-              />
-            </Box>
-          </Grid>
-          <Grid
-            size={{ xs: 12, sm: 12, md: 5, lg: 5, xl: 5 }}
-            sx={{
-              display: "flex",
-              justifyContent: "flex-start",
-              alignItems: "center",
-            }}
-          >
-            <Typography variant="h6">
-              Our mission is to make disciples who are deeply connected to
-              Christ and each other; who are empowered by the Holy Spirit to
-              love boldly; who are equipped to serve as conduits of God's love
-              to a broken world.
-            </Typography>
-          </Grid>
-        </Grid>
-      </Grid>
-    </Grid>
+    </>
   );
 };
 
